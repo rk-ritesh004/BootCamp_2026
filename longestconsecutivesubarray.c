d6b8bb90fe4e4bf53530d0ce5c6ee8f858b14fca
rk-ritesh004 <guptaritesh36686@gmail.com>
2026-05-28 11:57:43 +0530

    longest consecutive subarray

---
+++ longestconsecutivesubarray.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int count=1,max=1;
+    for(int i=1;i<n;i++){
+        if(arr[i]>arr[i-1]){
+            count++;
+        }else{
+            count=1;
+        }
+        if(count>max){
+            max=count;
+        }
+    }
+    printf("%d",max);
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,4 +1,4 @@
- // for(int i=0;i<n;i++){
-    //     d1+=arr[i][i];
-    //     d2+=arr[i][n-1-i];
-    // }
+
+            max=count;
+        }
+    }
