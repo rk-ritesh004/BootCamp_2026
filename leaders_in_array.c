158fa4dedc1946dad81317a32742b3d9a5069222
Ritesh Kumar Gupta <guptaritesh36686@gmail.com>
2026-05-28 10:32:16 +0530

    Add files via upload

---
+++ leaders_in_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Leaders in the array are: ");
+    int max = arr[n-1];
+    printf("%d ",max);
+    for(int i=n-2;i>=0;i--){
+        if(arr[i]>max){
+            printf("%d ",arr[i]);
+            max = arr[i];
+        }
+    }
+}
